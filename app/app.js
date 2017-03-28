db372d33fc29a5d3071c6097c74d5c0a59752f32
Greg Duncan <gregdd@outlook.com>
2017-03-28 11:42:34 +1000

    add basic express set up

--- app/app.js
+++ app/app.js
@@ -1,10 +1,22 @@
-let http = require('http');
+let express = require('express');
+let app = express();
 
-let myServer = http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<h1>Greg\'s Profile</h1>');
-    res.end();
+app.get('/', function (req, res) {
+    res.send('<h1>Greg and his Projects</h1>');
 });
 
-myServer.listen(3000);
-console.log('goto localhost: 3000!!!');
+let server = app.listen(3000, function () {
+    console.log('go to localhost:3000');
+});
+
+
+// let http = require('http');
+
+// let myServer = http.createServer(function (req, res) {
+//     res.writeHead(200, {"Content-Type": "text/html"});
+//     res.write('<h1>Greg\'s Profile</h1>');
+//     res.end();
+// });
+
+// myServer.listen(3000);
+// console.log('goto localhost: 3000!!!');

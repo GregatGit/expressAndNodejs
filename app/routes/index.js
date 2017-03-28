9baa5215b24fe4b15f2f641f1b0ee20eee02da4f
Greg Duncan <gregdd@outlook.com>
2017-03-28 16:01:47 +1000

    set up local and global variable to use in ejs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,10 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {
+        pageTitle: 'Home',
+        pageID: 'home'
+    });
 });
 
 module.exports = router;

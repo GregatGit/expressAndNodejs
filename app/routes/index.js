4a1e9d304e74f43148850c657981e8fd9a4d967a
Greg Duncan <gregdd@outlook.com>
2017-03-28 13:58:37 +1000

    use express.static to access images all over the app

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,9 @@ let router = express.Router();
 
 router.get('/', function (req, res) {
     res.send(`
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <h1>Welcome to Greg's Projects</h1>
+        <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
         <p>Hi - this is a work in progress to show my Projects</p>
     `);
 });

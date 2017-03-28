fa57fc67c8ab3c7b81773f74ba35d47a2f83511e
Greg Duncan <gregdd@outlook.com>
2017-03-28 12:25:57 +1000

    added simple routes using express

--- app/app.js
+++ app/app.js
@@ -5,6 +5,13 @@ let dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
+    res.send(`
+        <h1>Welcome to Greg's Projects</h1>
+        <p>Hi - this is a work in progress to show my Projects</p>
+    `);
+});
+
+app.get('/speakers', function (req, res) {
     let info = '';
     dataFile.speakers.forEach(item => {
         info += `
@@ -17,6 +24,15 @@ app.get('/', function (req, res) {
     res.send(`
         <h1>Greg and his Projects</h1>
         ${info}
+    `);
+});
+
+app.get('/speakers/:speakerid', function (req, res) {
+    let speaker = dataFile.speakers[req.params.speakerid];
+    res.send(`
+        <h1>Here is your speaker</h1>
+        <h2>${speaker.name}</h2>
+        <p>${speaker.summary}</p>
     `);
 });
 

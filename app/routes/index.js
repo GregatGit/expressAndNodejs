fde1027c764b40cbd2167e4fe74b558d1c6b3f18
Greg Duncan <gregdd@outlook.com>
2017-03-28 14:40:08 +1000

    installed ejs and set up a views folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send(`
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <h1>Welcome to Greg's Projects</h1>
-        <img src="/images/misc/background.jpg" alt="background" style="height: 300px;">
-        <p>Hi there- this is a work in progress to show my Projects</p>
-        <p>see if reload works</p>
-        <script src="/reload/reload.js></script>
-    `);
+    res.render('index');
 });
 
 module.exports = router;

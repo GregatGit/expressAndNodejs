cbd464727aa1bb1aeb7eeecb83b252cbfee93163
Greg Duncan <gregdd@outlook.com>
2017-03-28 18:10:27 +1000

    use variables in the ejs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,15 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', function (req, res) {
+    let data = req.app.get('appData');
+    let pagePhotos = [];
+
+    data.speakers.forEach(item => {
+        pagePhotos = pagePhotos.concat(item.artwork);
+    });
     res.render('index', {
         pageTitle: 'Home',
+        artwork: pagePhotos,
         pageID: 'home'
     });
 });

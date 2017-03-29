81aaf29e3254157ab73c6ba0c666987010fd8813
Greg Duncan <gregdd@outlook.com>
2017-03-29 14:21:55 +1000

    use if statement in ejs for diff bootstrap classes

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,12 +5,13 @@ let router = express.Router();
 router.get('/', function (req, res) {
     let data = req.app.get('appData');
     let pagePhotos = [];
-
+    let pageSpeakers = data.speakers;
     data.speakers.forEach(item => {
         pagePhotos = pagePhotos.concat(item.artwork);
     });
     res.render('index', {
         pageTitle: 'Home',
+        speakers: pageSpeakers,
         artwork: pagePhotos,
         pageID: 'home'
     });

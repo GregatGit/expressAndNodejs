d751c34f17c089dca72695e054816a561c260331
Greg Duncan <gregdd@outlook.com>
2017-03-29 13:51:31 +1000

    use include in ejs to break up html into partials

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -2,23 +2,36 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/speakers', function (req, res) {
-    let info = '';
-    let dataFile = req.app.get('appData');
-    dataFile.speakers.forEach(item => {
-        info += `
-            <li>
-                <h2>${item.name}</h2>
-                <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker" >
-                <p>${item.summary}</p>
-            </li>
-        `;
+    let data = req.app.get('appData');
+    let pagePhotos = [];
+    let pageSpeakers = data.speakers;
+
+    data.speakers.forEach(item => {
+        pagePhotos = pagePhotos.concat(item.artwork);
     });
-    res.send(`
-        <link rel="stylesheet" type="text/css" href="/css/style.css">
-        <h1>Greg and his Projects</h1>
-        ${info}
-        <script src="/reload/reload.js></script>
-    `);
+    res.render('speakers', {
+        pageTitle: 'Speakers',
+        artwork: pagePhotos,
+        speakers: pageSpeakers,
+        pageID: 'speakers'
+    });
+    // let info = '';
+    // let dataFile = req.app.get('appData');
+    // dataFile.speakers.forEach(item => {
+    //     info += `
+    //         <li>
+    //             <h2>${item.name}</h2>
+    //             <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker" >
+    //             <p>${item.summary}</p>
+    //         </li>
+    //     `;
+    // });
+    // res.send(`
+    //     <link rel="stylesheet" type="text/css" href="/css/style.css">
+    //     <h1>Greg and his Projects</h1>
+    //     ${info}
+    //     <script src="/reload/reload.js></script>
+    // `);
 });
 
 router.get('/speakers/:speakerid', function (req, res) {

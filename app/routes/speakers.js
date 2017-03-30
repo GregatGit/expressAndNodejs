bff46fa4bafc27b1a32f5101d54d81ce004359de
Greg Duncan <gregdd@outlook.com>
2017-03-30 14:44:14 +1000

    insert speakers into the drop down

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -13,7 +13,7 @@ router.get('/speakers', function (req, res) {
         pageTitle: 'Speakers',
         artwork: pagePhotos,
         speakers: pageSpeakers,
-        pageID: 'speakers'
+        pageID: 'speakerList'
     });
     // let info = '';
     // let dataFile = req.app.get('appData');
@@ -35,16 +35,32 @@ router.get('/speakers', function (req, res) {
 });
 
 router.get('/speakers/:speakerid', function (req, res) {
-    let dataFile = req.app.get('appData');
-    let speaker = dataFile.speakers[req.params.speakerid];
-    res.send(`
-        <link rel="stylesheet" type="text/css" href="/css/style.css">
-        <h1>${speaker.title}</h1>
-        <h2>${speaker.name}</h2>
-        <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker" >
-        <p>${speaker.summary}</p>
-        <script src="/reload/reload.js></script>
-    `);
+    let data = req.app.get('appData');
+    let pagePhotos = [];
+    let pageSpeakers = [];
+
+    data.speakers.forEach(item => {
+        if (item.shortname === req.params.speakerid){
+            pageSpeakers.push(item);
+            pagePhotos = pagePhotos.concat(item.artwork);
+        }
+    });
+    res.render('speakers', {
+        pageTitle: 'Speaker Info',
+        artwork: pagePhotos,
+        speakers: pageSpeakers,
+        pageID: 'speakerDetail'
+    });
+    // let dataFile = req.app.get('appData');
+    // let speaker = dataFile.speakers[req.params.speakerid];
+    // res.send(`
+    //     <link rel="stylesheet" type="text/css" href="/css/style.css">
+    //     <h1>${speaker.title}</h1>
+    //     <h2>${speaker.name}</h2>
+    //     <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker" >
+    //     <p>${speaker.summary}</p>
+    //     <script src="/reload/reload.js></script>
+    // `);
 });
 
 module.exports = router;
